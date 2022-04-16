a4cc187001626fb065374f7b35ad3aeb862983eb
Denis Sidorevich <2floppi@gmail.com>
2022-04-16 23:33:41 +0300

    test2

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function test(){
-    console("test");
-}
+var token = "908022839:AAEdPzMwOIRUcrOH9xcxwc8BPrS9gVw17w8";
+var telegramUrl = "https://api.telegram.org/bot" + token;
+
+function getWebhookInfo() {
+    var url = telegramUrl + "/getWebhookInfo";
+    var response = UrlFetchApp.fetch(url);
+    Logger.log(response);
+    }
